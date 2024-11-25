c10f77d86bb65870fd2a3f0cc2ccc2bd8f9b2356
yilololo <yilololo@YilongdeMacBook-Air.local>
2024-11-25 11:52:56 +0800

    new

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='0.1',
     py_modules=['main'],
     packages=find_packages(),
+    package_data={
+        'arco_deploy': ['*.yaml']
+    }
+    include_package_data=True,
     install_requires=[
         'fabric',
         'click',
