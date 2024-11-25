9a0eed1b358ece5d258fa379be53380100651e55
yilololo <yilololo@YilongdeMacBook-Air.local>
2024-11-25 14:16:49 +0800

    new

--- setup.py
+++ setup.py
@@ -2,23 +2,36 @@ from setuptools import setup, find_packages
 
 setup(
     name='arco',
-    version='0.1',
-    py_modules=['arco_tool'],
+    version='0.1.0',
+    description='A deployment tool for Arco services',
+    author='yilo',
+    author_email='lylhaha01@outlook.com',
     packages=find_packages(),
     package_data={
-        '': ['*.yml']
+        'config': ['*.yml'],
     },
     include_package_data=True,
     install_requires=[
-        'fabric',
-        'click',
-        'loguru',
-        'omegaconf'
+        'fabric>=2.5.0',
+        'click>=7.0',
+        'loguru>=0.5.0',
+        'omegaconf>=2.1.0',
     ],
     entry_points={
         'console_scripts': [
-            'arco-deploy=arco_deploy:main',
-            'arco-init=arco_init:main'
+            'arco-deploy=arco_deploy.arco_deploy:main',
+            'arco-init=arco_init.arco_init:main'
         ]
     },
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
